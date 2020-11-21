8aff243d6357c584ce19ce99d24e898321dbb836
playerdecuple <playerdecuple@gmail.com>
2020-11-21 22:49:32 +0900

    Update test

---
+++ src/test/java/io/Test/Test.java
@@ -0,0 +1,13 @@
+package io.Test;
+
+import com.DecupleProject.Hangeul;
+
+public class Test {
+
+    public static void main(String args[]) {
+
+        System.out.println(Hangeul.qwertyToHangeul("dlrj gksmfk sjan glaemfdjTek"));
+
+    }
+
+}
